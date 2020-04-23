2a06b28031dd3688177b537ba6194d427fc1c365
thedudefreebird <tyler.freebery@gmail.com>
2020-04-23 13:24:37 -0400

    Initial Commit for edited Nodes

---
+++ src/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validateBearerToken')
+const errorHandler = require('./errorHandler')
+const bookmarksRouter = require('./bookmarksRouter')
+
+const app = express()
+
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test'
+}))
+app.use(helmet())
+app.use(cors())
+app.use(validateBearerToken)
+app.use(bookmarksRouter)
+
+app.get('/', (req,res) => {
+    res.send('Hello, world!')
+})
+app.use(errorHandler)
+module.exports = app
